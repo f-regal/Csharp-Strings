4600871f2c9c6f4a0cf6fb9165a5a02add56b50a
Faysal Regal <faysalregal@gmail.com>
2022-08-06 21:20:15 +0100

    Manipulated text using diff string methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CsharpCourse
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "mosh hamedani ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+            Console.WriteLine("Full Name: '{0}'", fullName);
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);   
+            Console.WriteLine(firstName + ' ' + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("mosh", "moshfegh"));
+            
+            if(String.IsNullOrEmpty("") || String.IsNullOrEmpty(null))
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            var str = "25";
+            var age = Convert.ToInt32(str); //Can also use Convert.ToByte because int is 4 bytes and we only need 1 byte to store a number less than 250
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));
+        }
+    }
+}
